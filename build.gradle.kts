aa551074d8c012faf17b4eec8a0f8c99737de9ab
alysson <alysson.cirilo@gmail.com>
2026-02-27 16:16:16 -0300

    daemon toolchain

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,7 @@ plugins {
     alias(libs.plugins.android.kotlin.multiplatform.library) apply false
     alias(libs.plugins.kotlinMultiplatform) apply  false
 }
+
+tasks.named<UpdateDaemonJvm>("updateDaemonJvm") {
+    languageVersion = JavaLanguageVersion.of(libs.versions.gradleDaemonJvmToolchain.get().toInt())
+}
